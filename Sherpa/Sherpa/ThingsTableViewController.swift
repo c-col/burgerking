84c74ca4104b6daf663e2a307ff90d1903640339
Meg Grasse <Meg@Megs-MacBook-Pro.local>
2015-09-12 23:45:25 -0500

    Adding location

--- Sherpa/Sherpa/ThingsTableViewController.swift
+++ Sherpa/Sherpa/ThingsTableViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import CoreLocation
 
-class ThingsTableViewController: UITableViewController, CLL {
+class ThingsTableViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +40,45 @@ class ThingsTableViewController: UITableViewController, CLL {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         return 0
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+    {
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil)
+            {
+                println("Error: " + error.localizedDescription)
+                return
+            }
+            
+            if placemarks.count > 0
+            {
+                let pm = placemarks[0] as! CLPlacemark
+                self.displayLocationInfo(pm)
+            }
+            else
+            {
+                println("Error with the data.")
+            }
+        })
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark)
+    {
+        
+        self.locationManager.stopUpdatingLocation()
+        println(placemark.locality)
+        println(placemark.postalCode)
+        println(placemark.administrativeArea)
+        println(placemark.country)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!)
+    {
+        println("Error: " + error.localizedDescription)
     }
     
     /*
