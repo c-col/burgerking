1f8fd60403e870fcd8a68b6a59485e6d897ad4c0
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-09-12 21:17:20 -0500

    Added FeedTableCell

--- Sherpa/Sherpa/FeedTableCell.swift
+++ Sherpa/Sherpa/FeedTableCell.swift
@@ -1,13 +1,13 @@
 //
-//  FeedTableViewController.swift
+//  FeedTableCell.swift
 //  Sherpa
 //
 //  Created by Aaron Leon on 9/12/15.
 //  Copyright (c) 2015 Burger King. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 
-class FeedTableViewController: UITableViewController {
-    
+class FeedTableCell: UITableViewCell {
+
 }
